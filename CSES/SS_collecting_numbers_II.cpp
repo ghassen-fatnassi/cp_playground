a55b3aa91c3af6606cdaf40459eb9f9b00b0b63f
ghassen-fatnassi <oneinbillion77@gmail.com>
2024-06-11 23:28:11 +0100

    good enoug?

---
+++ CSES/SS_collecting_numbers_II.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define FASTIO  \
+    cin.tie(0); \
+    ios_base::sync_with_stdio(false);
+#define TC while (t--)
+const ll MOD = 998244353;
+
+void solve()
+{
+}
+
+int main()
+{
+    FASTIO
+    solve();
+    return 0;
+}
--- CURR_CONTEST.cpp
+++ CURR_CONTEST.cpp
@@ -5,12 +5,45 @@ using namespace std;
     cin.tie(0); \
     ios_base::sync_with_stdio(false);
 #define TC while (t--)
-const int MOD = 1e9 + 7;
+// #define MAXN 1e9
+// vector<int> spf(MAXN + 1, 1);
+const ll MOD = 998244353;
 
 void solve()
 {
-    ll n;
-    cin >> n;
+    ll h, n;
+    cin >> h >> n;
+    ll a[n];
+    ll c[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i];
+    }
+    ll l = 1;
+    ll r = 4e10;
+    ll mid = (l + r) / 2;
+    while (l < r)
+    {
+        mid = (l + r) / 2;
+        ll damage = 0;
+        for (int i = 0; i < n; i++)
+        {
+            damage += a[i] * (mid / c[i] + (mid % c[i] != 0));
+        }
+        if (damage >= h)
+        {
+            r = mid; // Corrected line
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    cout << r << "\n";
 }
 
 int main()
