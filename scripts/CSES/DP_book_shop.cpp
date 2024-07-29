bc0130bfeca9c3c6f971a779f2bfa258ca644f8a
ghassen-fatnassi <oneinbillion77@gmail.com>
2024-07-29 18:55:47 +0100

    solved DP_book_shop

--- scripts/CSES/DP_book_shop.cpp
+++ scripts/CSES/DP_book_shop.cpp
@@ -7,7 +7,40 @@ using namespace std;
 #define TC while (t--)
 const int MOD = 1e9 + 7;
 
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    ll h[n]; // prices
+    ll s[n]; // pages
+    for (int i = 0; i < n; i++)
+    {
+        cin >> h[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i];
+    }
+    ll dp[x + 1] = {0}; // dp[i] is the max pages we can get for amount of money i
+    for (int i = 0; i < x + 1; i++)
+    {
+        dp[i] = 0;
+    }
+    for (int j = 0; j < n; j++) // going through all the prices in an ordered way, to make sure to not repeat the same book twice
+    {
+        for (int i = x; i > -1; i--) // checking the contributions of each book to the max number of pages we can get at a certain amount of money i
+        {
+            if (i >= h[j])
+            {
+                dp[i] = max(dp[i], dp[i - h[j]] + s[j]);
+            }
+        }
+    }
+    cout << dp[x] << "\n";
+}
 int main()
 {
     FASTIO
+    solve();
+    return 0;
 }
--- scripts/CURR_CONTEST.cpp
+++ scripts/CURR_CONTEST.cpp
@@ -9,28 +9,40 @@ const ll MOD = 998244353;
 
 void solve()
 {
-    int n, k;
-    cin >> n >> k;
-    int occ = 0;
-    int a[2 * n - 1];
-    for (int i = 0; i < 2 * n - 1; i += 2)
-    {
-        a[i] = (i + 1) / 2;
-        a[i + 1] = (i + 1) / 2;
+    ll n;
+    cin>>n;
+    multiset<ll> a;
+    bool odd=false;
+    bool even=false;
+    for(int i=0;i<n;i++){
+        ll x;
+        cin>>x;
+        if(x%2==0){even=true;}else{odd=true;}
+        if(even && odd){cout<<-1;return;}
+        a.insert(x);
     }
-    for (int i = 0; i < 2 * n - 1; i++)
-    {
-        if (k <= 0)
-        {
-            break;
-        }
-        else
-        {
-            k = k - (n - i);
-            occ++;
+
+    ll count=0;
+    vector<ll> ops;
+    while(count<=40){
+        ll big=*a.rbegin();
+        ll small=*a.begin();
+        ll mid=(big+small)/2;
+        if(mid==0){break;}
+        else{
+            multiset<ll> new_a;
+            for(auto elm:a){
+                new_a.insert(abs(elm-mid));
+            }
+            a = new_a;
+            count++;
+            ops.push_back(mid);
         }
     }
-    cout << occ << "\n";
+    cout<<count<<"\n";
+    for(auto elm:ops){
+        cout<<elm<<" ";
+    }
 }
 
 int main()
@@ -41,6 +53,7 @@ int main()
     TC
     {
         solve();
+        cout<<"\n";
     }
     return 0;
 }
