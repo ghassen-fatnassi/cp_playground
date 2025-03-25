85556a08eed9be6f70e712ec2563998f203c586e
ghassen-fatnassi <oneinbillion77@gmail.com>
2025-03-25 01:26:48 +0100

    finished implementing binary lifting

--- scripts/Templates/Tree.cpp
+++ scripts/Templates/Tree.cpp
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-// this class is supposed to contain the following methods
-// int n;cin>>n; to get the number of nodes in the tree
-// Tree T(n); to construct the tree
-// T.add(u,v) *k(pseudocode) ; insert k edges in the form of u,v where u is the parent of v
-// T.initialize(x) ; where x is the node to be considered the root throughout the code
-// T.lca(u,v) ; this returns the least common ancestor between u and v
-// T.dist(u,v) ; this returns the distance between u and v in terms of edges (throughout the shortest path)
+// all of the commendts are mine btw :D
+//  this class is supposed to contain the following methods
+//  int n;cin>>n; to get the number of nodes in the tree
+//  Tree T(n); to construct the tree
+//  T.add(u,v) *k(pseudocode) ; insert k edges in the form of u,v where u is the parent of v
+//  T.initialize(x) ; where x is the node to be considered the root throughout the code
+//  T.lca(u,v) ; this returns the least common ancestor between u and v
+//  T.dist(u,v) ; this returns the distance between u and v in terms of edges (throughout the shortest path)
 class Tree
 {
 public:
-    vector<vector<int>> adj;
-    vector<int> parent;
-    vector<vector<int>> dp;
-    int size;
-    int log_size;
+    vector<vector<int>> adj; // the main DS for the tree
+    vector<int> parent;      // will help in dfs
+    vector<int> height;      // will be useful in making nodes same height
+    vector<vector<int>> dp;  // the main DS for binary lifting
+    int size;                // number of nodes in the tree
+    int log_size;            // (int)ceil(log(numer of nodes))
     Tree(int n)
     {
         size = n;
         adj.resize(size);
-        parent.resize(size);
+        parent.resize(size, -1);
+        height.resize(size, 0);
         log_size = (int)ceil(log(size));
-        dp.resize(n, vector<int>(log_size));
+        dp.resize(n, vector<int>(log_size, 0)); // init to 0 since it's gonna be the root node
     }
     void add(int u, int v)
     {
@@ -34,17 +36,12 @@ public:
     void init()
     {
         parent[0] = 0;
-        for (int i = 0; i < log_size; i++)
-        {
-            dp[0][i] = 0;
-        }
         dp_dfs(0);
     }
     void dp_dfs(int x)
     {
-        parent[x] = x;
         dp[x][0] = parent[x];
-        for (int j = log_size; j > 0; j--)
+        for (int j = 1; j <= log_size; j--)
         {
             dp[x][j] = dp[dp[x][j - 1]][j - 1]; // the "node up 2^j from x" is the "node up 2^(j-1) from the node up 2^(j-1) from x"
         }
@@ -52,19 +49,46 @@ public:
         {
             if (parent[x] != y)
             {
-                // logic here
+                parent[y] = x;
+                height[y] = height[x] + 1;
                 dp_dfs(y);
             }
         }
     }
-    void lca(int u, int v)
+    int lca(int u, int v)
     {
-        // fix the problem of u and v not being same height
-        // check case of u and v are same node after fixing height(v in the subtree of u or vice versa)
-        // now suppose u and v same height and keep going
+        //  fix the problem of u and v not being same height
+        if (height[v] > height[u])
+        {
+            swap(u, v);
+        }
+        // from now on we'll suppose height[u]>==height[v]
+        for (int i = log_size; i >= 0; i--)
+        {
+            if ((1 << i) <= height[u] - height[v])
+            {
+                u = dp[u][i];
+            }
+        }
+        //  check case of u and v are same node after fixing height(v in the subtree of u or vice versa)
+        if (u == v)
+        {
+            return u;
+        }
+        //  now suppose u and v same height and keep going
+        for (int i = log_size; i >= 0; i--)
+        {
+            if (dp[u][i] != dp[v][i])
+            {
+                u = dp[u][i];
+                v = dp[v][i];
+            }
+        }
+        return parent[u];
     }
-    void dist(int u, int v)
+    int dist(int u, int v)
     {
         // finds the distance (length of path ,in edges) between 2 nodes in the tree
+        return 2 * height[lca(u, v)] - height[u] - height[v];
     }
 };
