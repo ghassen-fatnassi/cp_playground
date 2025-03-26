25c8dfef633714ec08cfe20008b9df6f8ce84ffe
ghassen-fatnassi <oneinbillion77@gmail.com>
2025-03-26 12:03:25 +0100

    today i actually learned something about trees , there are 2 nodes in a tree whose one of them is guaranteed to be the furthest from any other node in the tree , they are the endpoints of the tree diameter

---
+++ scripts/CSES/TREE_distances_I.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define ll long long
+const int MOD = 998244353;
+
+void dfs(int node, int parent, vector<vector<int>> &adj, vector<int> &dist)
+{
+    for (int child : adj[node])
+    {
+        if (child == parent)
+        {
+            continue;
+        }
+        dist[child] = dist[node] + 1;
+        dfs(child, node, adj, dist);
+    }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n - 1; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<int> distA(n), distB(n);
+    distA[0] = 0;
+    dfs(0, -1, adj, distA);
+
+    int A = max_element(distA.begin(), distA.end()) - distA.begin();
+    distA[A] = 0;
+    dfs(A, -1, adj, distA);
+
+    int B = max_element(distA.begin(), distA.end()) - distA.begin();
+    distB[B] = 0;
+    dfs(B, -1, adj, distB);
+    for (int i = 0; i < n; i++)
+    {
+        cout << max(distB[i], distA[i]) << " ";
+    }
+}
+
+int main()
+{
+    FASTIO
+    int t = 1;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
--- scripts/CSES/TREE_radius.cpp
+++ scripts/CSES/TREE_radius.cpp
@@ -1,57 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define FASTIO                        \
     ios_base::sync_with_stdio(false); \
     cin.tie(0);
 #define ll long long
+
 const int MOD = 998244353;
 
-void dfs(int node, int parent, vector<vector<int>> &adj, vector<bool> &matched, int &matchings)
+pair<int, int> dfs(int node, int parent, vector<vector<int>> &adj)
 {
-    for (int child : adj[node])
+    int max = -1;
+    int index = node;
+    for (auto child : adj[node])
     {
         if (child == parent)
         {
             continue;
         }
-
-        dfs(child, node, adj, matched, matchings);
-
-        if (!matched[node] && !matched[child])
+        auto out = dfs(child, node, adj);
+        if (out.second > max)
         {
-            matched[node] = matched[child] = true;
-            matchings++;
+            index = out.first;
+            max = out.second;
         }
     }
+    return {index, max + 1};
 }
 
 void solve()
 {
     int n;
     cin >> n;
-
-    vector<vector<int>> adj(n + 1);
+    vector<vector<int>> adj(n);
     for (int i = 0; i < n - 1; i++)
     {
         int u, v;
         cin >> u >> v;
+        u--, v--;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-
-    vector<bool> matched(n + 1, false);
-    int matchings = 0;
-
-    dfs(1, -1, adj, matched, matchings);
-
-    cout << matchings << endl;
+    auto Apair = dfs(0, -1, adj);
+    auto Bpair = dfs(Apair.first, -1, adj);
+    cout << Bpair.second << endl;
 }
 
 int main()
 {
     FASTIO
     int t = 1;
+    // cin>>t;
     while (t--)
     {
         solve();
